d38a18e3109f6dfccbd485d46dbd0bb4ab71a990
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-09-10 12:04:10 -0300

    feat(backend): add POST create goal route

--- backend/src/http/app.ts
+++ backend/src/http/app.ts
@@ -1,3 +1,33 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoal } from '../use-cases/create-goal'
+import z from 'zod'
 
-export const app = fastify()
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
