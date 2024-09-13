59523c5daf56f2e68427b47d5742b2112d1b5932
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-09-13 09:50:52 -0300

    fix(frontend): adjust dependencies installations

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,14 +1,41 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
+import { QueryClientProvider, useQuery } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 // import { EmptyGoals } from './components/empty-goals'
 
+type SummaryResponse = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    } 
+  >
+}
+
 export function App() {
+  const { data } = useQuery<SummaryResponse>({
+    queryKey: ['summary'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/summary')
+      const data = response.json()
+
+      return data
+    }
+  })
+
   return (
-    <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
-      <CreateGoal />
-    </Dialog>
+    <QueryClientProvider client={queryClient}>
+      <Dialog>
+        {/* <EmptyGoals /> */}
+        <Summary />
+        <CreateGoal />
+      </Dialog>
+    </QueryClientProvider>
   )
 }
