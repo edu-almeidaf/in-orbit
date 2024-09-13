61c3f0a0fe06a17a30cf09c44220cf41f38c8645
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-09-13 10:09:25 -0300

    refactor(frontend): Add condition for render the empty state for the goals  or not

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,28 +1,21 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
-import { QueryClientProvider, useQuery } from '@tanstack/react-query'
-import { queryClient } from './lib/react-query'
-// import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
+import { EmptyGoals } from './components/empty-goals'
 
 export function App() {
   const { data } = useQuery({
     queryKey: ['summary'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3333/summary')
-      const data = response.json()
-
-      return data
-    },
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
   })
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Dialog>
-        {/* <EmptyGoals /> */}
-        <Summary />
-        <CreateGoal />
-      </Dialog>
-    </QueryClientProvider>
+    <Dialog>
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+      <CreateGoal />
+    </Dialog>
   )
 }
